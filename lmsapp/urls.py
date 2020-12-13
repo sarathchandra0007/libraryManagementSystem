f8c3a34328c17f86b40eca617e94b5758e145733
Sarathchanrda <sarathchandra0007@gmail.com>
2020-12-13 11:27:35 +0530

    Added user login logout and registration functionalities

---
+++ lmsapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+from django.urls import path, include
+from django.conf import settings
+from . import views
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
---
+++ lmsapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserRegisterForm
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+
+
+def index(request):
+    return render(request, 'index.html', {'title': 'index'})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST) or None
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form, 'title': 'reqister here'})
+
+
+def login_user(request):
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            form = login(request, user)
+            messages.success(request, f' wecome {username} !!')
+            return redirect('index')
+        else:
+            messages.info(request, f'account done not exit plz sign in')
+    form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form, 'title': 'log in'})
